b5eef2704191af04811f9a72f196a30e0eac0b07
jazzalin <jazzalin.main@gmail.com>
2017-02-18 14:03:20 -0500

    rk4.f now deprecated

--- rk4.f
+++ rk4.f
@@ -1,5 +1,14 @@
 !James Azzalini - 4th order Runge Kutta method implementation
-      program rk4
+!DEPRECATED
+        module parameters
+              implicit none
+
+              real, parameter :: m1 = 1.5, m2 = 1, L1 = 0.7, L2 = 0.45
+              real, parameter :: g = 9.81
+
+        end module parameters
+
+        program rk4
               implicit none
 
               interface
@@ -21,13 +30,22 @@
               t = ti
               do while (t .lt. tf)
                 ! RK4 code
+                h1 = f(t,y)
+                h2 = f(t + 0.5D0 * dt, y + 0.5D0 * dt * h1)
+                h3 = f(t + 0.5D0 * dt, y + 0.5D0 * dt * h2)
+                h4 = f(t + dt, y + dt * h3)
+                y = y + dt * (h1 + 2.0D0 * (h2 + h3) + h4)/ 6.0D0
                 t = t + dt
               end do
       end program rk4
 
       function f (t,y)
+              use parameters
               implicit none
+
               real(kind=kind(1.0D0)), intent(in) :: y,t
               real(kind=kind(1.0D0)) :: f
+              real(kind=kind(1.0D0)) :: th1,th2,w1,w2
+                        
               ! function expression
       end function 
